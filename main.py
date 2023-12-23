e00032da655c658e188cae7c857d43a90fa212ce
Erfan Moradi <84937954+ErfanMoradi2000@users.noreply.github.com>
2023-12-24 01:41:07 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-import pandas as pd
-from sklearn.cluster import KMeans
-
-dataset = pd.read_csv('data/creditcard.csv')
-
-X = dataset.iloc[:, :-1]
-y = dataset.iloc[:, -1]
-
-model = KMeans(n_clusters=2, n_init=10)
-model.fit(X=X)
-y_predicted = model.predict(X)
-
-n = (y == y_predicted).sum()
-
+if __name__ == '__main__':
+    pass
